26a16cab982b766ae2a4374dac840507a09c36b7
Mourad Omar <Mouradadel1919@gmail.com>
2026-02-25 01:33:36 +0200

    Final Commit

--- src/models/enums/ProductsEnums.py
+++ src/models/enums/ProductsEnums.py
@@ -2,6 +2,6 @@ from enum import Enum
 
 class ProductEnums(Enum):
     
-    PRODUCTS_FILE_PATH="./helpers/scraping/unique_products.json"
+    PRODUCTS_FILE_PATH="./helpers/scraping/unique_products2.json"
 
  
